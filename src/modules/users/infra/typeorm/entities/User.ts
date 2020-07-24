493846987831f6801b68c46379b93c9524e50fca
Matheus Rufino <contato@matheusrufino.com.br>
2020-07-24 20:20:55 -0300

    Create S3 Storage Provider

--- src/modules/users/infra/typeorm/entities/User.ts
+++ src/modules/users/infra/typeorm/entities/User.ts
@@ -5,7 +5,7 @@ import {
   CreateDateColumn,
   UpdateDateColumn,
 } from 'typeorm';
-
+import uploadConfig from '@config/upload';
 import { Exclude, Expose } from 'class-transformer';
 
 @Entity('users')
@@ -34,9 +34,18 @@ class User {
 
   @Expose({ name: 'avatar_url' })
   get getAvatarUrl(): string | null {
-    return this.avatar
-      ? `${process.env.APP_API_URL}/files/${this.avatar}`
-      : null;
+    if (!this.avatar) {
+      return null;
+    }
+
+    switch (uploadConfig.driver) {
+      case 'disk':
+        return `${process.env.APP_API_URL}/files/${this.avatar}`;
+      case 's3':
+        return `https://${uploadConfig.config.disk.aws.bucket}.s3.us-east-2.amazonaws.com/${this.avatar}`;
+      default:
+        return null;
+    }
   }
 }
 
--- src/shared/container/providers/StorageProvider/index.ts
+++ src/shared/container/providers/StorageProvider/index.ts
@@ -1,14 +1,17 @@
 import { container } from 'tsyringe';
+import uploadConfig from '@config/upload';
 
 import IStorageProvider from './models/IStorageProvider';
 
-import DiskStorgeProvider from './implementations/DiskStorageProvider';
+import DiskStorageProvider from './implementations/DiskStorageProvider';
+import S3StorageProvider from './implementations/S3StorageProvider';
 
 const providers = {
-  disk: DiskStorgeProvider,
+  disk: DiskStorageProvider,
+  s3: S3StorageProvider,
 };
 
 container.registerSingleton<IStorageProvider>(
-  'DiskStorgeProvider',
-  providers.disk,
+  'StorageProvider',
+  providers[uploadConfig.driver],
 );
